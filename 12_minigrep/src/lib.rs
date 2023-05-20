7b92b15bc67ec9466dbe3341f431a222aeff0b30
Eran Riesenfeld <er@madidea.net>
2023-05-20 18:06:46 +0200

    Fixing some code in minigrep as adviced by chapter 13

--- 12_minigrep/src/lib.rs
+++ 12_minigrep/src/lib.rs
@@ -7,20 +7,25 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments: minigrep -- needle heystack");
-        }
-        Ok(Config::new(args))
-    }
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config::new(&query, &file_path, ignore_case))
+    }
+    fn new(query: &str, file_path: &str, ignore_case: bool) -> Config {
         Config {
-            query,
-            file_path,
+            query: String::from(query),
+            file_path: String::from(file_path),
             ignore_case,
         }
     }
@@ -42,25 +47,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- 12_minigrep/src/main.rs
+++ 12_minigrep/src/main.rs
@@ -1,10 +1,9 @@
-use std::process;
+use std::{env, process};
 
 use minigrep::{run, Config};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
