1c68c730a6c8bbc8523fd37616c4cebb5c0a36bc
Eran Riesenfeld <er@madidea.net>
2023-05-20 19:17:16 +0200

    started with chapter 5

---
+++ 15_smart_pointers/src/main.rs
@@ -0,0 +1,19 @@
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    dbg!(list);
+}
+#[derive(Debug)]
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
